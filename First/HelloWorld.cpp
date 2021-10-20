da0fc61800fb38cddf0eef4cfe6b25e6af04c667
robakerson <robakerson@gmail.com>
2021-10-19 22:37:03 -0500

    cpp syntax v0

---
+++ First/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float annualSalary = 50000.99;
+    float monthlySalary = annualSalary / 12;
+    cout<<"Your monthly salary is" << monthlySalary;
+
+
+    return 0;
+}
