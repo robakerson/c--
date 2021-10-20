312e73c0a96bb3270256033bb0e2493c2e269ec0
robakerson <robakerson@gmail.com>
2021-10-19 23:43:48 -0500

    data types

--- First/HelloWorld.cpp
+++ First/HelloWorld.cpp
@@ -3,15 +3,30 @@ using namespace std;
 
 int main(){
 
-    // Declare float
-    float annualSalary;
-    // Console in and assign to annualSalary
-    cout << "Please enter your annual salary:";
-    cin >> annualSalary;
-    float monthlySalary = annualSalary / 12;
-    // console out
-    cout << "Your monthly salary is " << monthlySalary;
+    // Data types
 
+    int year = 1995;
+    char gender = 'f'; // one character
+    bool isOlderThan18 = true;
+    float averageGrade = 4.5;
+    double balance = 456498415645; // double has twice memory size as float
+
+    cout << "Int min value is " << INT_MIN << endl;
+    cout << "Int max value is " << INT_MAX << endl;
+    cout << "Size of unsigned int is " << sizeof(unsigned int) << "bytes\n";
+    cout << "UInt max value is " << UINT_MAX << endl;
+    
+
+    // // Declare float
+    // float annualSalary;
+    // // Console in and assign to annualSalary
+    // cout << "Please enter your annual salary:";
+    // cin >> annualSalary;
+    // float monthlySalary = annualSalary / 12;
+    // // console out
+    // cout << "Your monthly salary is " << monthlySalary;
+
+    // char character = 'a'; //character names need single quotes
 
     return 0;
 }
